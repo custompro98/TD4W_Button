8501b6b8cf833c6ed7d9894e783991471f13915d
Mitch Joa <mitchjoa@gmail.com>
2014-06-14 00:09:39 -0400

    Initial Commit

---
+++ app/src/main/java/com/custompro98/td4w_button/app/MainActivity.java
@@ -0,0 +1,51 @@
+package com.custompro98.td4w_button.app;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton td4w_button = (ImageButton)this.findViewById(R.id.td4w_button);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.td4w);
+
+        td4w_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
